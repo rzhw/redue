2aea177a397263a63ab30f73c43bc8d3fd5ab499
Richard Wang <zhwang@live.com.au>
2016-02-03 14:56:52 +1100

    Split reminders parsing to a new RemindersManager class

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,8 +4,13 @@ import {TimeAgoPipe} from 'angular2-moment';
 import {NavbarComponent} from './navbar.component';
 import {NavbarTab} from './navbartab';
 import {Reminder} from './reminder';
+import {RemindersManager} from './remindersmanager';
 
 declare var jQuery: JQueryStatic;
+
+var fs = require('fs');
+var path = require('path');
+var os = require('os');
 
 @Component({
   selector: 'dueinator-app',
@@ -21,75 +26,28 @@ declare var jQuery: JQueryStatic;
   </ul>`
 })
 export class AppComponent {
+  public remindersManager: RemindersManager;
   public reminders: Reminder[];
   public selectedTab: NavbarTab;
-  private allReminders: Reminder[];
 
   constructor() {
-    // TODO move all this code elsewhere
-    var dueTuples = (function() {
-      var fs = require('fs');
-      var path = require('path');
-      var os = require('os');
-      var zlib = require('zlib');
-      var bplist = require('bplist-parser');
-      var plist = require('simple-plist');
+    // Get Dropbox folder path. Currently assumes personal, FIXME
+    // Future: Don't use synchronous functions
+    var dbInfoPath = path.join(os.homedir(), '.dropbox/info.json');
+    var dbInfoJson = fs.readFileSync(dbInfoPath);
+    var dbInfo = JSON.parse(dbInfoJson);
+    var dbPath = dbInfo.personal.path;
 
-      // Get Dropbox folder path. Currently assumes personal, FIXME
-      // Future: Don't use synchronous functions
-      var dbInfoPath = path.join(os.homedir(), '.dropbox/info.json');
-      var dbInfoJson = fs.readFileSync(dbInfoPath);
-      var dbInfo = JSON.parse(dbInfoJson);
-      var dbPath = dbInfo.personal.path;
-
-      // Get Due file
-      var duePath = path.join(dbPath, 'Apps/Due App/Sync.dueappgz');
-      var dueGzBuf = fs.readFileSync(duePath);
-      var dueBplistBuf = zlib.unzipSync(dueGzBuf);
-      var dueBplistObj = bplist.parseBuffer(dueBplistBuf);
-      var duePlistDict = dueBplistObj[0];
-      var dueObjects = duePlistDict["$objects"];
-      console.log(dueObjects);
-
-      // Find Due Reminder objects.
-      var dueReminders = [];
-      for (var i = 0; i < dueObjects.length; i++) {
-        if (dueObjects[i].hasOwnProperty("$class")
-            && dueObjects[i]["$class"]["UID"] == 16) { // magic number FIXME
-          dueReminders.push(dueObjects[i]);
-        }
-      }
-      console.log(dueReminders);
-
-      // TODO move this elsewhere.
-      var nsTimeToDate = function(time: number) {
-        var d = new Date(time * 1000 + Date.UTC(2001, 1, 1));
-        // For some reason the above date is off by a month and two days?
-        // TODO investigate
-        d.setMonth(d.getMonth() - 1);
-        d.setDate(d.getDate() - 2);
-        return d;
-      };
-
-      // Now go through this subset. We can combine this code w/above FIXME
-      return dueReminders.map(function(x) {
-        return <Reminder>{
-          name: <string>dueObjects[x["name"]["UID"]],
-          dateDue: nsTimeToDate(dueObjects[x["dateDue"]["UID"]]["NS.time"]),
-          status: <number|BigInteger>x["status"],
-          data: x
-        };
-      })
-    })();
-
-    console.log(dueTuples);
-
-    this.allReminders = dueTuples;
-    this.reminders = [];
+    // Get Due file
+    var duePath = path.join(dbPath, 'Apps/Due App/Sync.dueappgz');
+    this.remindersManager = RemindersManager.fromPath(duePath);
+    console.log('RemindersManager initialised');
+    console.log(this.remindersManager.allReminders);
   }
 
   onSelectedTabChanged(tab: NavbarTab) {
-    this.reminders = this.allReminders.filter(reminder => {
+    // TODO this should be in RemindersManager
+    this.reminders = this.remindersManager.allReminders.filter(reminder => {
       return tab.filter(reminder.status);
     }).sort((a, b) => {
       if (a.dateDue < b.dateDue) {
---
+++ app/remindersmanager.ts
@@ -0,0 +1,60 @@
+import {Reminder} from './reminder';
+
+var fs = require('fs');
+var path = require('path');
+var os = require('os');
+var zlib = require('zlib');
+var bplist = require('bplist-parser');
+var plist = require('simple-plist');
+
+export class RemindersManager {
+  public allReminders: Reminder[];
+  public currentReminders: Reminder[];
+
+  constructor(reminders: Reminder[]) {
+    this.allReminders = reminders;
+    this.currentReminders = [];
+  }
+
+  // TODO consider moving this into a RemindersParser or similarly named class
+  static fromPath(pathStr: string) {
+    var dueGzBuf = fs.readFileSync(pathStr);
+    var dueBplistBuf = zlib.unzipSync(dueGzBuf);
+    var dueBplistObj = bplist.parseBuffer(dueBplistBuf);
+    var duePlistDict = dueBplistObj[0];
+    var dueObjects = duePlistDict["$objects"];
+    console.log(dueObjects);
+
+    // Find Due Reminder objects.
+    var dueReminders = [];
+    for (var i = 0; i < dueObjects.length; i++) {
+      if (dueObjects[i].hasOwnProperty("$class")
+          && dueObjects[i]["$class"]["UID"] == 16) { // magic number FIXME
+        dueReminders.push(dueObjects[i]);
+      }
+    }
+    console.log(dueReminders);
+
+    // TODO move this elsewhere.
+    var nsTimeToDate = function(time: number) {
+      var d = new Date(time * 1000 + Date.UTC(2001, 1, 1));
+      // For some reason the above date is off by a month and two days?
+      // TODO investigate
+      d.setMonth(d.getMonth() - 1);
+      d.setDate(d.getDate() - 2);
+      return d;
+    };
+
+    // Now go through this subset. We can combine this code w/above FIXME
+    var reminders = dueReminders.map(function(x) {
+      return <Reminder>{
+        name: <string>dueObjects[x["name"]["UID"]],
+        dateDue: nsTimeToDate(dueObjects[x["dateDue"]["UID"]]["NS.time"]),
+        status: <number|BigInteger>x["status"],
+        data: x
+      };
+    });
+
+    return new RemindersManager(reminders);
+  }
+}
